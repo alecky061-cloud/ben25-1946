5e2ab30bf2c64905ddcd34f65ad804a6c2082ec4
alecky061-cloud <alecky061@gmail.com>
2026-04-08 08:43:06 +0300

    Update display.php

--- display.php
+++ display.php
@@ -1,1 +1,14 @@
+<?php
+include 'db_connect.php';
 
+$result = $conn->query("SELECT * FROM users");
+
+while ($row = $result->fetch_assoc()) {
+    echo "First Name: " . $row['first_name'] . "<br>";
+    echo "Last Name: " . $row['last_name'] . "<br>";
+    echo "Email: " . $row['email'] . "<br>";
+    echo "Password: " . $row['password'] . "<br><br>";
+}
+
+$conn->close();
+?>
